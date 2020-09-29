47d23405f2ff39631b8c8fbc554163013cb51511
BlueAlien99 <45181394+BlueAlien99@users.noreply.github.com>
2020-09-29 22:26:18 +0200

    "Register" module

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Register from './components/Register';
 
 function App(){
   return (
-    <div>
-      Hello
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </Router>
   );
 }
 
